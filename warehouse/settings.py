ca80a4fd26a45ffe862054d3d9527a79e26beaf1
Pawel Mazurkiewicz <mailbox.mazu@gmail.com>
2023-05-24 17:22:57 +0200

    deployment version

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v8bofe1!jns6#$q^tlq@!k_ws0_*0!inht&n-s4&!p*cke1hw1"
+SECRET_KEY = os.getenv("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
